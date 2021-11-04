88694ea3f4d248f93d4b24ba327ade0c06e6ea54
Benson Jiang <benson.jiang@sjsu.edu>
2021-11-04 13:19:40 -0700

    added models, created database

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,14 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 myapp_obj = Flask(__name__)
 myapp_obj.config.from_mapping(
-	SECRET_KEY = 'you-will-never-guess'
-)
+	SECRET_KEY = 'you-will-never-guess',
+	SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db'), SQLALCHEMY_TRACK_MODIFICATION = False)
 
-from app import routes
+db = SQLAlchemy(myapp_obj)
+
+from app import routes, models
---
+++ app/models.py
@@ -0,0 +1,9 @@
+from app  import db
+
+class city(db.Model):
+	id = db.Column(db.Integer, primary_key = True)
+	city = db.Column(db.String(64), index = True, unique = True)
+	rank = db.Column(db.Integer, index = True, unique = True)
+
+	def __repr__(self):
+		return f'<City: {self.city}>'
