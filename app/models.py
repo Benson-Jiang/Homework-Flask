1e02286ec6d2202c59700e5cb5f845bbbd28b253
Benson Jiang <benson.jiang@sjsu.edu>
2021-11-08 18:57:39 -0800

    added list of inputed cities and set up database

--- app/models.py
+++ app/models.py
@@ -3,7 +3,7 @@ from app  import db
 class city(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	city = db.Column(db.String(64), index = True, unique = True)
-	rank = db.Column(db.Integer, index = True, unique = True)
-
+	rank = db.Column(db.Integer, index = True)
+	visit = db.Column(db.Boolean)
 	def __repr__(self):
 		return f'<City: {self.city}>'
--- app/routes.py
+++ app/routes.py
@@ -1,14 +1,20 @@
 from app import myapp_obj
 from flask import render_template, redirect, flash
 from app.forms import TopCities
+from app.models import city
+from app import db
 
 @myapp_obj.route("/", methods = ['GET', 'POST'])
 def home():
 	title = 'Top Cities'
 	name = 'Benson'
 	form = TopCities()
+	list = city.query.order_by(city.rank).all()
 	if form.validate_on_submit():
 		flash(f'City name: {form.city_name.data} has been added')
+		c = city(city = form.city_name.data, rank = form.city_rank.data, visit = form.is_visited.data)
+		db.session.add(c)
+		db.session.commit()
+		list = city.query.order_by(city.rank).all()
 		return redirect("/")
-	return render_template('home.html', title = title, name = name, form = form)
-
+	return render_template('home.html', title = title, name = name, form = form, list = list)
